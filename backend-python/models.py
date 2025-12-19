c0de68d310ec1c74255fbe484b784fb2837735ad
Alyosha <alohahoy@gmail.com>
2025-12-19 00:05:35 +0000

    build basic features for "scaled" version of the project

--- backend-python/models.py
+++ backend-python/models.py
@@ -2,30 +2,13 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Dict
 
-class CodingChallenge(BaseModel):
+class User(BaseModel):
+    username: str
+
+class Challenge(BaseModel):
     id: int
     points: int
-    topic: str
-
-
-class User(BaseModel):
-    id: int
-    username: str
-    email: str
-
 
 class Submission(BaseModel):
-    id: int
     user: User
-    challenge: CodingChallenge
-    time_to_complete: int
-    submitted_at: datetime
-
-
-class Analytics(BaseModel):
-    total_submissions: int
-    avg_completion: int
-
-
-    # mapping of topic to amount of submissions (Bar graph)
-    topics: Dict[str, int]
+    challenge: Challenge
